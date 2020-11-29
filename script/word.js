02af4168dd43a0c97953fa100dad161d51252262
Excel404 <65623910+Excel404@users.noreply.github.com>
2020-11-29 22:59:56 +0100

    fifth commit

--- script/word.js
+++ script/word.js
@@ -1,206 +1,154 @@
 function Word(){
-    var self = this;    
-    self.wordInterface = document.createElement("div");
-    self.wordInterface.setAttribute("class","word-interface");
-    self.topBar = document.createElement('div');
-    self.topBar.setAttribute('class','top-bar');
-    self.toolBar = document.createElement('div');
-    self.toolBar.setAttribute('class','tool-bar');
-    self.menuBar = document.createElement('div');
-    self.menuBar.setAttribute('class','menu-bar');
-    self.searchBox = document.createElement('input');
-    self.searchBox.setAttribute('class','search-box');
-    self.searchBox.setAttribute('type','text');
-    self.searchBox.setAttribute('placeholder','type to search');
-    self.textArea = document.createElement('textarea');
-    self.textArea.setAttribute('class','text-area');
-    self.textArea.setAttribute('type','text');
+    //var this = this;    
+   this.wordInterface = document.getElementById("word-interface");
+   this.topBar = document.getElementById("Top-baR");;
+   this.toolBar = document.getElementById("Tool-baR");
+   this.menuBar = document.getElementById("Menu-baR");
+   this.searchBox = document.createElement('input');
+   this.searchBox.setAttribute('class','search-box');
+   this.searchBox.setAttribute('type','text');
+   this.searchBox.setAttribute('placeholder','type to search');
+   this.textArea = document.querySelector("#Text-areA");
 
-    self.file = document.createElement('span');
-    self.file.setAttribute('class','file');
-    self.file.innerHTML = "FILE";
-    self.home = document.createElement('span');
-    self.home.setAttribute('class','home');
-    self.home.innerHTML = "HOME";
-    self.Insert = document.createElement('span');
-    self.Insert.setAttribute('class','insert');
-    self.Insert.innerHTML = "INSERT";
-    self.Design = document.createElement('span');
-    self.Design.setAttribute('class','design');
-    self.Design.innerHTML = "DESIGN";
-    self.pageLayout = document.createElement('span');
-    self.pageLayout.setAttribute('class','page-layout');
-    self.pageLayout.innerHTML = "PAGE LAYOUT";
-    self.references = document.createElement('span');
-    self.references.setAttribute('class','references');
-    self.references.innerHTML = "REFERENCES";
-    self.mailings = document.createElement('span');
-    self.mailings.setAttribute('class','mailings');
-    self.mailings.innerHTML = "MAILINGS";
-    self.review = document.createElement('span');
-    self.review.setAttribute('class','review');
-    self.review.innerHTML = "REVIEW";
-    self.view = document.createElement('span');
-    self.view.setAttribute('class','view');
-    self.view.innerHTML = "VIEW";
+   this.file = document.getElementById("FilE");;
+   this.file.innerHTML = "FILE";
+   this.home = document.getElementById("HomE");
+   this.home.innerHTML = "HOME";
+   this.Insert = document.getElementById("InserT");
+   this.Insert.innerHTML = "INSERT";
+   this.Design = document.getElementById("DesigN");
+   this.Design.innerHTML = "DESIGN";
+   this.pageLayout = document.getElementById("Page-layouT");
+   this.pageLayout.innerHTML = "PAGE LAYOUT";
+   this.references = document.getElementById("ReferenceS");
+   this.references.innerHTML = "REFERENCES";
+   this.mailings = document.getElementById("MailingS");
+   this.mailings.innerHTML = "MAILINGS";
+   this.review = document.getElementById("RevieW");
+   this.review.innerHTML = "REVIEW";
+   this.view = document.getElementById("VieW");
+   this.view.innerHTML = "VIEW";
     
-    self.home.addEventListener('click',()=>{
+   this.home.addEventListener('click',()=>{
+        this.homeInterface = document.getElementById('Home-interfacE');
+        this.homeInterface.style.display = 'block';
+        //this.home.style.color = 'blue';
+        //this.home.style.backgroundColor = 'white';
         Home();
-        self.home.style.color = 'blue';
-        self.home.style.backgroundColor = 'whitesmoke';
     })
-    self.Insert.addEventListener('click',()=>{
-        Insert();
-        self.Insert.style.color = 'blue';
-        self.Insert.style.backgroundColor = 'whitesmoke';
+   this.Insert.addEventListener('click',()=>{
+        //Insert();
+       //this.Insert.style.color = 'blue';
+       //this.Insert.style.backgroundColor = 'white';
     })
-    self.Design.addEventListener('click',()=>{
+   this.Design.addEventListener('click',()=>{
         //Home();
-        self.Design.style.color = 'blue';
-        self.Design.style.backgroundColor = 'whitesmoke';
+       //this.Design.style.color = 'blue';
+       //this.Design.style.backgroundColor = 'white';
     })
-    self.pageLayout.addEventListener('click',()=>{
+   this.pageLayout.addEventListener('click',()=>{
         //Home();
-        self.pageLayout.style.color = 'blue';
-        self.pageLayout.style.backgroundColor = 'whitesmoke';
+       //this.pageLayout.style.color = 'blue';
+       //this.pageLayout.style.backgroundColor = 'white';
     })
-    self.references.addEventListener('click',()=>{
+   this.references.addEventListener('click',()=>{
         //Home();
-        self.references.style.color = 'blue';
-        self.references.style.backgroundColor = 'whitesmoke';
+       //this.references.style.color = 'blue';
+       //this.references.style.backgroundColor = 'white';
     })
-    self.mailings.addEventListener('click',()=>{
+   this.mailings.addEventListener('click',()=>{
         //Home();
-        self.mailings.style.color = 'blue';
-        self.mailings.style.backgroundColor = 'whitesmoke';
+       //this.mailings.style.color = 'blue';
+       //this.mailings.style.backgroundColor = 'white';
         
     })
-    self.review.addEventListener('click',()=>{
+   this.review.addEventListener('click',()=>{
         //Home();
-        self.review.style.color = 'blue';
-        self.review.style.backgroundColor = 'whitesmoke';
+       //this.review.style.color = 'blue';
+       //this.review.style.backgroundColor = 'white';
     })
-    self.view.addEventListener('click',()=>{
+   this.view.addEventListener('click',()=>{
         //Home();
-        self.view.style.color = 'blue';
-        self.view.style.backgroundColor = 'whitesmoke';
+       //this.view.style.color = 'blue';
+       //this.view.style.backgroundColor = 'white';
     })
 
-    //self.replace.addEventListener('onclick',()=>{    })
+    
     function Home(){
-        self.replace = document.createElement('span');
-        self.replace.setAttribute('class','replace');
-        self.replace.innerHTML = "Replace";
-        self.caseIcon = document.createElement('span');
-        self.caseIcon.innerHTML ='Aa<i></i>';
+
+       this.replace = document.getElementById('ReplacE');
+       this.replace.addEventListener('click',Replace)
+       this.caseIcon = document.getElementById('Case-icoN');
         function Replace(){
-            self.replaceInterface = document.createElement('div');
-            self.replaceInterface.setAttribute('class','replace-interface');
-            self.replaceTopBar = document.createElement('div');
-            self.replaceTopBar.setAttribute('class','replace-top-bar');
-            self.findLabel = document.createElement('span');
-            self.findLabel.setAttribute('class','find-label');
-            self.findLabel.innerHTML= "Find what:";
-            self.findBox = document.createElement('input');
-            self.findBox.setAttribute('class','find-box');
-            self.findBox.setAttribute('type','text');
-            self.replaceLabel = document.createElement('span');
-            self.replaceLabel.setAttribute('class','replace-label');
-            self.replaceLabel.innerHTML= 'Replace what:';
-            self.replaceBox = document.createElement('input');
-            self.replaceBox.setAttribute('class','replace-box');
-            self.replaceBox.setAttribute('type','text');
-            var replaceBtn = document.createElement('button');
-            replaceBtn.setAttribute('class','replace-button');
-            replaceBtn.innerHTML = "Replace";
-            var replaceAllBtn = document.createElement('button');
-            replaceAllBtn.setAttribute('class','replace-all-button');
-            replaceAllBtn.innerHTML ="Replace All";
-            
+            this.replaceInterface = document.getElementById('Replace-interfacE');
+            this.replaceInterface.style.display = "block";
+            var replaceBtn = document.getElementById('Replace-buttoN');
+            var replaceAllBtn = document.getElementById('Replace-all-buttoN');
+
             replaceBtn.addEventListener('click',()=>{
-                self.textArea.value = self.textArea.value.replace(RegExp(self.findBox.value,'i'),self.replaceBox.value);
-                console.log(self.findBox.value);
-                console.log(self.replaceBox.value);
-                console.log(self.textArea.value);
+                document.querySelector("#Text-areA").value = document.querySelector("#Text-areA").value.replace(RegExp(document.querySelector('#Find-boX').value,'i'),document.querySelector('#Replace-boX').value);
             });
             replaceAllBtn.addEventListener('click', function(){
-                self.textArea.value = self.textArea.value.replace(RegExp(self.findBox.value, 'gi'),self.replaceBox.value);
+                document.querySelector("#Text-areA").value = document.querySelector("#Text-areA").value.replace(RegExp(document.querySelector('#Find-boX').value,'gi'),document.querySelector('#Replace-boX').value);
             });
-            self.replace.style.backgroundColor = 'lightblue';
-            self.replaceInterface.appendChild(self.replaceTopBar);
-            self.replaceInterface.appendChild(self.findLabel);
-            self.replaceInterface.appendChild(self.findBox);
-            self.replaceInterface.appendChild(self.replaceLabel);
-            self.replaceInterface.appendChild(self.replaceBox);
-            self.replaceInterface.appendChild(replaceBtn);
-            self.replaceInterface.appendChild(replaceAllBtn);
-            self.wordInterface.appendChild(self.replaceInterface);
-            
         }
         function Bold(){
-            self.bold.style.backgroundColor = 'lightblue';
-            //self.textArea.value = ;
+           //this.bold.style.backgroundColor = 'lightblue';
+            //this.textArea.value = ;
             
-            var selectedText = self.textArea.value;
-            var lie = '';
-            if(self.textArea.selectionStart == self.textArea.selectionEnd){
+            var selectedText = document.querySelector("#Text-areA");
+            //var lie = document.querySelector("#Text-areA").value;
+            if(document.querySelector("#Text-areA").selectionStart == document.querySelector("#Text-areA").selectionEnd){
                 return;
-                //lie = selectedText.getSelection();
             }
-            var selected = self.textArea.value.slice(self.textArea.selectionStart, self.textArea.selectionEnd);
-            //selected.style.fontWeight ='bold';
-            console.log(selected.strike());
-            self.textArea.setRangeText(selected, self.textArea.selectionStart,self.textArea.selectionEnd, "end");
+            var selected = document.querySelector("#Text-areA").value.slice(document.querySelector("#Text-areA").selectionStart,document.querySelector("#Text-areA").selectionEnd);
+            selectedText.style.fontStyle ='italic';
+            console.log(selected.italics());
+            /*document.querySelector("#Text-areA").setRangeText(selected.strike);
+            */
             console.log(selectedText);
-            console.log(lie);
+            //console.log(lie);
         }
         function ChangeCase(){
-            
-            if(self.textArea.selectionStart == self.textArea.selectionEnd){
-                let end = self.textArea.value;
-                let look = end.slice(end.lastIndexOf(' '),self.textArea.selectionStart);
-                //self.textArea.setRangeText(look.toUpperCase());    
-                let beg = look.toUpperCase();
-                end.replace(new RegExp(look),beg);
-                console.log(beg);
+            var text = document.querySelector("#Text-areA");
+            var text1 = text.value;
+            var end =text.selectionEnd;
+            var start =text.selectionStart;
+            //let upperCase = 
+            if(text.selectionStart == text.selectionEnd){
+                let previousIndex = text1.lastIndexOf(' ', start-1);
+                console.log("start"+start);
+                console.log("previousIndex"+previousIndex);
+                console.log("end"+end)
+                let nextIndex = text1.indexOf(" ", end);
+                console.log(nextIndex);
+                const begin = previousIndex < 0 ? 0: previousIndex +1;
+                const end1 = nextIndex < 0 ? text1.length : nextIndex +1;
+                const between = text1.substring(begin, end1);
+                console.log(between.toUpperCase());
+                text1.replace(RegExp(between,'i'), between.toUpperCase());
+
             }
-            var text = self.textArea.value.slice(self.textArea.selectionStart,self.textArea.selectionEnd);
-            self.textArea.setRangeText(text.toUpperCase());
+
+            var text3 = text1.slice(start,end);
+            text.setRangeText(text3.toUpperCase());
 
 
         }
-        self.bold = document.createElement('span');
-        self.bold.setAttribute('class','bold-icon');
-        self.bold.innerHTML = '<b>B</b>';
-        self.caseIcon.addEventListener('click', ChangeCase)
-        self.bold.addEventListener('click', Bold);
-        self.replace.addEventListener('click', Replace);
-        self.menuBar.appendChild(self.caseIcon);
-        self.menuBar.appendChild(self.bold);
-        self.menuBar.appendChild(self.replace);
+       this.bold = document.getElementById('Bold-icoN')
+       this.bold.setAttribute('class','bold-icon');
+       this.caseIcon.addEventListener('click', ChangeCase)
+       this.bold.addEventListener('click', Bold);
+       this.replace.addEventListener('click', Replace);
     }
     function Insert(){
-        self.dateTime = document.createElement('span');
-        self.dateTime.setAttribute('class','date-time');
-        self.dateTime.innerHTML = 'Date and Time';
-        self.insertMenu = document.createElement('div');
-        self.insertMenu.appendChild(self.dateTime);
-        self.menuBar.innerHTML = self.insertMenu;
+       this.dateTime = document.createElement('span');
+       this.dateTime.setAttribute('class','date-time');
+       this.dateTime.innerHTML = 'Date and Time';
+       this.insertMenu = document.createElement('div');
+       this.insertMenu.appendChild(this.dateTime);
+       this.menuBar.innerHTML =this.insertMenu;
     }
-    self.toolBar.appendChild(self.file);
-    self.toolBar.appendChild(self.home);
-    self.toolBar.appendChild(self.Insert);
-    self.toolBar.appendChild(self.Design);
-    self.toolBar.appendChild(self.pageLayout);
-    self.toolBar.appendChild(self.references);
-    self.toolBar.appendChild(self.mailings);
-    self.toolBar.appendChild(self.review);
-    self.toolBar.appendChild(self.view);
-    self.wordInterface.appendChild(self.topBar);
-    self.wordInterface.appendChild(self.toolBar);
-    
-    self.wordInterface.appendChild(self.menuBar);
-    self.wordInterface.appendChild(self.textArea);
-    document.body.appendChild(self.wordInterface);
+   
 }
 var word1 = new Word();
